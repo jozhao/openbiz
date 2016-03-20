23416c8bc44eae06fdc797fb6ff4bf13dc6e0056
Joseph Zhao <joseph.zhao@xing.net.au>
2016-03-20 12:54:49 +1100

    Add open biz_bsae theme

---
+++ themes/openbiz/openbiz_base/template.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @file
+ * template.php
+ */
+
+// Auto-rebuild the theme registry during theme development.
+if (theme_get_setting('openbiz_base_rebuild_registry') && !defined('MAINTENANCE_MODE')) {
+  // Rebuild .info data.
+  system_rebuild_theme_data();
+  // Rebuild theme registry.
+  drupal_theme_rebuild();
+}
+
+/**
+ * Implements HOOK_theme().
+ */
+function openbiz_base_theme(&$existing, $type, $theme, $path) {
+  include_once './' . drupal_get_path('theme', 'openbiz_base') . '/includes/registry.inc';
+  return _openbiz_base_theme($existing, $type, $theme, $path);
+}
+
+/**
+ * Clear any previously set element_info() static cache.
+ *
+ * If element_info() was invoked before the theme was fully initialized, this
+ * can cause the theme's alter hook to not be invoked.
+ *
+ * @see https://www.drupal.org/node/2351731
+ */
+drupal_static_reset('element_info');
+
+/**
+ * Include hook_preprocess_*() hooks.
+ */
+include_once './' . drupal_get_path('theme', 'openbiz_base') . '/includes/preprocess.inc';
+
+/**
+ * Include hook_process_*() hooks.
+ */
+include_once './' . drupal_get_path('theme', 'openbiz_base') . '/includes/process.inc';
+
+/**
+ * Include hook_*_alter() hooks.
+ */
+include_once './' . drupal_get_path('theme', 'openbiz_base') . '/includes/alter.inc';
---
+++ themes/openbiz/openbiz_base/theme-settings.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @file
+ * theme-settings.php
+ */
+
+/**
+ * Include theme common function.
+ */
+include_once './' . drupal_get_path('theme', 'openbiz_base') . '/includes/common.inc';
+
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ * @param $form
+ * @param $form_state
+ * @param null $form_id
+ */
+function openbiz_base_form_system_theme_settings_alter(&$form, $form_state, $form_id = NULL) {
+  // @see https://drupal.org/node/943212
+  $theme = !empty($form_state['build_info']['args'][0]) ? $form_state['build_info']['args'][0] : FALSE;
+  if (isset($form_id) || $theme === FALSE || !in_array('openbiz_base', _openbiz_base_get_base_themes($theme, TRUE))) {
+    return;
+  }
+
+  // Accessibility and support settings
+  $form['support'] = array(
+    '#type'          => 'fieldset',
+    '#title'         => t('Accessibility and support settings'),
+  );
+
+  $form['support']['openbiz_base_skip_link_anchor'] = array(
+    '#type'          => 'textfield',
+    '#title'         => t('Anchor ID for the “skip link”'),
+    '#default_value' => theme_get_setting('openbiz_base_skip_link_anchor'),
+    '#field_prefix'  => '#',
+    '#description'   => t('Specify the HTML ID of the element that the accessible-but-hidden “skip link” should link to. Note: that element should have the <code>tabindex="-1"</code> attribute to prevent an accessibility bug in webkit browsers. (<a href="!link">Read more about skip links</a>.)', array('!link' => 'https://drupal.org/node/467976')),
+  );
+
+  $form['support']['openbiz_base_skip_link_text'] = array(
+    '#type'          => 'textfield',
+    '#title'         => t('Text for the “skip link”'),
+    '#default_value' => theme_get_setting('openbiz_base_skip_link_text'),
+    '#description'   => t('For example: <em>Jump to navigation</em>, <em>Skip to content</em>'),
+  );
+
+  // Theme development settings
+  $form['development'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Theme development settings'),
+  );
+
+  $form['development']['openbiz_base_rebuild_registry'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Rebuild theme registry on every page.'),
+    '#default_value' => theme_get_setting('openbiz_base_rebuild_registry'),
+    '#description' => t('During theme development, it can be very useful to continuously <a href="!link">rebuild the theme registry</a>. WARNING: this is a huge performance penalty and must be turned off on production websites.', array('!link' => 'https://drupal.org/node/173880#theme-registry')),
+  );
+}
