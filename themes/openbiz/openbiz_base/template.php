5933d672d19a38f65622ef9f253d6162ca26a7c5
Joseph Zhao <joseph.zhao@xing.net.au>
2016-03-20 16:58:30 +1100

    Update openbiz_base theme

--- themes/openbiz/openbiz_base/template.php
+++ themes/openbiz/openbiz_base/template.php
@@ -5,6 +5,16 @@
  * template.php
  */
 
+/**
+ * Clear any previously set element_info() static cache.
+ *
+ * If element_info() was invoked before the theme was fully initialized, this
+ * can cause the theme's alter hook to not be invoked.
+ *
+ * @see https://www.drupal.org/node/2351731
+ */
+drupal_static_reset('element_info');
+
 // Auto-rebuild the theme registry during theme development.
 if (theme_get_setting('openbiz_base_rebuild_registry') && !defined('MAINTENANCE_MODE')) {
   // Rebuild .info data.
@@ -20,16 +30,6 @@ function openbiz_base_theme(&$existing, $type, $theme, $path) {
   include_once './' . drupal_get_path('theme', 'openbiz_base') . '/includes/registry.inc';
   return _openbiz_base_theme($existing, $type, $theme, $path);
 }
-
-/**
- * Clear any previously set element_info() static cache.
- *
- * If element_info() was invoked before the theme was fully initialized, this
- * can cause the theme's alter hook to not be invoked.
- *
- * @see https://www.drupal.org/node/2351731
- */
-drupal_static_reset('element_info');
 
 /**
  * Include hook_preprocess_*() hooks.
